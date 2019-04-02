09317d1fa942188b9f69878f90089e9077c79351
Beth Foster <beth.foster@uon.edu.au>
2019-04-02 23:08:53 +0000

    Secondary menu for Caesar cipher

--- hello.c
+++ hello.c
@@ -8,18 +8,32 @@ int main()
     cipher type they wanted*/
    int number=1;
    
-    printf("\tPlease select a Cipher\n");
+    printf("\tPlease select a Cipher\n"); /* '\t' makes the text tabbed and more presentable*/
     printf("\t~   1) Caesar Cipher       ~\n");
     printf("\t~   2) Substitution Cipher ~\n");
-    printf("\t~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
     /*scanf("%d\n", &number);*/ /*scan the users answer*/
 
     switch(number-1){
     case 0: printf("You have selected the Caesar Cipher!\n");
+    
+            printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+            
+            printf("Please Select a Caesar cipher task\n");
+            
+            printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+            
+            printf("\t Ceasar Cipher Option Menu\n"); 
+            printf("\t~   1)Encrypt English Code\n");
+            printf("\t~   2)Decrypt with an algorithm, cipher text and key\n");
+            printf("\t~   3)Decrypt with assumptions\n");
+            printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
             break;
     case 1: printf("You have Selected the Substitution Cipher!\n");
             break;
     default: printf("Selection not recognised please select 1 or 2!\n");
+    
+    
     }
 
             
