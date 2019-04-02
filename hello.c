3b3debd5c5d0ae855e1fb57dae4fc05dffb6a3cd
Beth Foster <beth.foster@uon.edu.au>
2019-04-02 22:48:50 +0000

    Buggy switch statement menu

--- hello.c
+++ hello.c
@@ -1,9 +1,25 @@
-/* Hello World program */
-
 #include<stdio.h>
 
 int main()
 {
-    printf("Hello World");
+/*.....................................*/
+    /* Began by creating an interactive 
+    MENU where the user could select the
+    cipher type they wanted*/
+   int number;
+   
+    printf("Please select a Cipher\n");
+    printf("1) Caesar Cipher\n");
+    printf("2) Substitution Cipher\n");
+    scanf("%d\n", &number); /*scan the users answer*/
 
+    switch(number-1){
+    case 0: printf("You have selected the Caesar Cipher\n");
+            break;
+    case 1: printf("You have Selected the Substitution Cipher\n");
+            break;
+    default: printf("Selection not recognised please select 1 or 2\n");
+    }
+
+            
 }
