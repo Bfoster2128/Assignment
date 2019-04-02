001d46b10c8c5744ac42317009d18ebbb1075618
Beth Foster <beth.foster@uon.edu.au>
2019-04-02 22:55:12 +0000

    Pretty switch statement menu

--- hello.c
+++ hello.c
@@ -6,19 +6,20 @@ int main()
     /* Began by creating an interactive 
     MENU where the user could select the
     cipher type they wanted*/
-   int number;
+   int number=1;
    
-    printf("Please select a Cipher\n");
-    printf("1) Caesar Cipher\n");
-    printf("2) Substitution Cipher\n");
-    scanf("%d\n", &number); /*scan the users answer*/
+    printf("\tPlease select a Cipher\n");
+    printf("\t~   1) Caesar Cipher       ~\n");
+    printf("\t~   2) Substitution Cipher ~\n");
+    printf("\t~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    /*scanf("%d\n", &number);*/ /*scan the users answer*/
 
     switch(number-1){
-    case 0: printf("You have selected the Caesar Cipher\n");
+    case 0: printf("You have selected the Caesar Cipher!\n");
             break;
-    case 1: printf("You have Selected the Substitution Cipher\n");
+    case 1: printf("You have Selected the Substitution Cipher!\n");
             break;
-    default: printf("Selection not recognised please select 1 or 2\n");
+    default: printf("Selection not recognised please select 1 or 2!\n");
     }
 
             
